b1d7df0f17bfc425bd202c4ff375ded6a015dfc4
Davide <davidelngyt@gmail.com>
2021-12-18 21:51:29 +0100

    style and script basics

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-/* 
-    - The `input` field is empty. The message for this error should say *"Whoops! It looks like you forgot to add your email"*
-    - The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Please provide a valid email address"*
-*/
+//The `input` field is empty. The message for this error should say *"Whoops! It looks like you forgot to add your email"*
+//The email address is not formatted correctly (i.e. a correct email address should have this structure: `name@host.tld`). The message for this error should say *"Please provide a valid email address"*
+
+const email = document.querySelector('#email');
+const btn = document.querySelector('#btn');
+
+const validEmail = (email) => {
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    return regex.test(String(email).toLowerCase());
+};
+
+btn.addEventListener('click', (e) => {
+    if (email.ariaValueMax.trim() == '') {
+        email.classList.add('invalid');
+    }
+
+    else if (!validEmail(email)) {
+        email.classList.add('invalid');
+    }
+
+    else {
+        email.classList.remove('invalid');
+    }
+});
